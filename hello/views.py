381044d68ccf445920aba1b4024f8521c02979f8
Bojie Jiang <jiangbojie@gmail.com>
2023-09-19 23:01:13 -0700

    3-3 more urls

--- hello/views.py
+++ hello/views.py
@@ -9,3 +9,29 @@ def hello_world(request):
 
 def hello_china(request):
     return HttpResponse("Hello China!")
+
+
+def hello_html(request):
+    """ 响应HTML内容 """
+    html = """
+    <html>
+        <body>
+            <h1 style="color:#f00">hello html</h1>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
+
+
+def article_list(request, month):
+    """
+    :param month: 今年某一个月的文章列表
+    """
+    return HttpResponse('article: {}'.format(month))
+
+
+def search(request):
+    """ GET参数的获取 """
+    name = request.GET.get('name', '')
+    print(name)
+    return HttpResponse('查询成功')
